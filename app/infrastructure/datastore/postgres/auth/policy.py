5240461bd26eeab97e47dcbd68bcdb7650e71b3e
Franco Posa <francojposa@gmail.com>
2019-05-07 18:00:59 -0500

    WIP implementing authorization policy

--- app/infrastructure/datastore/postgres/auth/policy.py
+++ app/infrastructure/datastore/postgres/auth/policy.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from aiohttp_security.abc import AbstractAuthorizationPolicy
 from passlib.hash import argon2
 
@@ -10,3 +12,14 @@ class PostgresAuthorizationPolicy(AbstractAuthorizationPolicy):
     ):
         self.user_client = user_client
         self.role_client = role_client
+
+    async def authorized_userid(self, identity: str) -> Optional[str]:
+        users = await self.user_client.select_where(
+            inclusion_map={"username": identity}, exclusion_map={"enabled": False}
+        )
+        if users:
+            return identity
+
+    async def permits(self, identity, permission, context=None):
+        if identity is None:
+            return False
